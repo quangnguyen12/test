bfd0b160a43d93e480b7808146770a2e38e8c3f2
quangnguyen12 <nguyenbaoquang1@gmail.com>
2022-02-13 22:59:12 +0700

    da viet hai dong comment

--- New Unity Project/Assets/test.cs
+++ New Unity Project/Assets/test.cs
@@ -7,12 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // bat dau thuc hien tro choi 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // update cai gi do 
     }
 }
